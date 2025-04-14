ac7db736b4a9b3e00dcda70bb0cd04787ae55a37
krishnathakkar29 <work.krishnat@gmail.com>
2025-04-14 20:29:33 +0530

    feat: 🎸 add mini project

---
+++ frontend (html-css-js)/index.js
@@ -0,0 +1,112 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+let password_1_El = document.getElementById("password_1_el");
+
+function generateCharacter() {
+  let random_nr = Math.floor(Math.random() * characters.length);
+  let random_item = characters[random_nr];
+  return random_item;
+}
+
+function generate_password() {
+  let i = 0;
+  const passwordLength = 12;
+  let password_1 = "";
+  while (i < passwordLength) {
+    password_1 += generateCharacter();
+    i++;
+  }
+  password_1_El.textContent = password_1;
+}
